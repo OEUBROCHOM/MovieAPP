e048738ac57fa6dd4f69f6ed1d8c518a9da3f40d
OEUB ROCHOM <oeub.rochom@student.passerellesnumeriques.org>
2025-08-25 16:44:15 +0700

    Add banner to favorite pages

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -5,10 +5,15 @@ import MovieCard from "../components/MovieCard";
 function Favorite(){
     const {favorites} = useMovieContext();
 
-    if (favorites) {
+    if (favorites && favorites.length > 0) {
         return (
             <div className="favorites">
-                <h2>My Favorite</h2>
+                <img className="favorites-banner" src="/images/banner2.jpg" alt="Favorites Banner" />
+
+                <div className="favorites-header">
+                    <h2>My Favorite</h2>
+                </div>
+
                 <div className="movies-grid">
                     {favorites.map((movie) => (
                         <MovieCard movie={movie} key={movie.id} />
@@ -17,9 +22,13 @@ function Favorite(){
             </div>
         );
     }
-    return <div className="favorites-empty">
-        <h2>No Favorite Movies Yet</h2>
-        <p>Start adding movies to your favorites and they will appear here</p>
-    </div>
+    return (
+        <div className="favorites-empty">
+            <img className="favorites-banner" src="/images/bannerNo.jpg" alt="Favorites Banner" />
+            <h2>No Favorite Movies Yet</h2>
+            <p>Start adding movies to your favorites and they will appear here</p>
+        </div>
+    );
 }
+
 export default Favorite;
